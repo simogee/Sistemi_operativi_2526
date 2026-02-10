478122d4a5148feef43fe573d76482b18ce0e619
Alg3rn0n <alg3rn0n@Alg3rn0n.Bmo>
2026-02-10 15:52:43 +0100

    aggiunti commenti e initialization.c

---
+++ phase2/initialization.c
@@ -0,0 +1,27 @@
+
+
+/**
+ * Variabili da dichiarare:
+ * Process count -> uint
+ * Soft-block count -> uint, numero di processi partiti ma non ancora terminati
+ * Ready queue -> coda di pcb in ready state quindi direi una list_head* 
+ * Current Process -> pcb_t* puntatore al processo attualmente in running state
+ * Device semaphore -> il kernel tiene un intero per ogni device esterno più uno per lo pseudo-clock. (Dal momento che i terminali sono due device indipendenti,
+ * il kernel tiene due semafori per ogni terminale.)
+ * 
+ * 
+ *   
+ * Bisogna inizializzare il Pass-Up Vector -> da vedere meglio (0x0FFF.F900)
+ * 
+ * 
+ * Inizializzare le strutture dati della phase1, includere quindi i file della fase precedente
+ * Inizializzare le strutture dati e variabili phase2 initPcbs() e initASL()
+ * 
+ * Load the system-wide interval Timer con 100 ms
+ * 
+ * Istanziare un process test
+ * 
+ * 
+ * Chiamare lo scheduler
+ * 
+ */
