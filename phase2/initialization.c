9e092629edc3b1fecd231e21541a248121cb8072
Alg3rn0n <alg3rn0n@Alg3rn0n.Bmo>
2026-02-21 18:03:59 +0100

    aggiunto il file p2test.c. inoltre per compilare correttamente dovete aggiornare il cmakelist.txt e dentro la sezione add_executable aggiungere i file phase2/.. da compilare. al momento ho commentato delle funzioni in initialization.c perchè non le abbiamo ancora create e sto cercando di fare debugging

--- phase2/initialization.c
+++ phase2/initialization.c
@@ -3,14 +3,14 @@
 
 #include "../headers/listx.h" // per poter usare list_head
 #include "../headers/const.h" // per poter usare le costanti al posto degli indirizzi SEMDEVLEN,PASSUPVECTOR
-#include "../headers/types.h" // per poter usare pcb_t 
+#include "../headers/types.h" // per poter usare pcb_t
 
 #include <uriscv/const.h>
 #include <uriscv/types.h>
-extern void uTLB_RefillHandler(), test(),exception_handler(); // funzioni provided esternamente e execption va ancora creata
+extern void uTLB_RefillHandler(), test(); // funzioni provided esternamente e execptionhandler andrà messa nel file exception
 const int PSEUDO_CLOCK_SEM_INDEX = SEMDEVLEN -1; // indirizzo fisso per lo pseudo-clock
 /**
- * 
+ *
  * Variabili da dichiarare:
  * Process count -> uint
  * Soft-block count -> uint, numero di processi partiti ma non ancora terminati
@@ -20,29 +20,29 @@ const int PSEUDO_CLOCK_SEM_INDEX = SEMDEVLEN -1; // indirizzo fisso per lo pseud
  * il kernel tiene due semafori per ogni terminale.): ogni device è associato ad un intero, quando un processo richiede un'op
  * i/o viene spostato da running alla coda relativa all'integer semaforo del dispositivo a cui viene fatta la richiesta e fatta al P.
  * Quando il dispositivo ha finito, invia l'interrupt e il processore quando riesce fa la V spostando il processo bloccato di nuovo nella coda.
- * 
- * 
- *   
+ *
+ *
+ *
  * Bisogna inizializzare il Pass-Up Vector -> il passup vector è un vettore i cui campi puntano alle funzioni interrupt handlers (indirizzo: 0x0FFFF900)
- * 
- * 
+ *
+ *
  * Inizializzare le strutture dati della phase1, includere quindi i file della fase precedente
- * 
+ *
  * Inizializzare le strutture dati e variabili phase2 initPcbs() e initASL()
- * 
+ *
  * Load the system-wide interval Timer con 100 ms -> no idea atm.
- * 
+ *
  * Istanziare un process test
- * 
- * 
+ *
+ *
  * Chiamare lo scheduler
- * 
+ *
  */
 
  /*init variabili */
  int process_counter; //Quanti processi attualmente presenti
  int soft_block_counter; //Quanti processi "Blocked" (ASL)
- struct list_head ready_queue; // coda dei processi 
+ struct list_head ready_queue; // coda dei processi
  pcb_t* current_process;
  int device_sem [SEMDEVLEN]; // un sem per device + 1 per pseudo-clock
  int* pseudo_clock_sem = &device_sem[PSEUDO_CLOCK_SEM_INDEX]; // questo indirizzo sarà solo per lo pseudoclock
@@ -51,10 +51,10 @@ int main(){
 
 /* inizializzazione del pass-up Vector la struttura passupvector_t si trova in usr/include/uriscv */
 passupvector_t* pass_up_vector       = (passupvector_t*) PASSUPVECTOR ;
- pass_up_vector->tlb_refill_handler   = (memaddr) uTLB_RefillHandler;
- pass_up_vector->tlb_refill_stackPtr = (memaddr) KERNELSTACK; // top della funzione
- pass_up_vector->exception_handler   = (memaddr) exception_handler;
- pass_up_vector->exception_stackPtr  = (memaddr) KERNELSTACK;
+ //pass_up_vector->tlb_refill_handler   = (memaddr) uTLB_RefillHandler;
+ //pass_up_vector->tlb_refill_stackPtr = (memaddr) KERNELSTACK; // top della funzione
+ //pass_up_vector->exception_handler   = (memaddr) exception_handler;
+ //pass_up_vector->exception_stackPtr  = (memaddr) KERNELSTACK;
 
  /*inizializzo strutture phase1*/
  initASL();
@@ -71,7 +71,7 @@ passupvector_t* pass_up_vector       = (passupvector_t*) PASSUPVECTOR ;
  }
 LDIT(PSECOND);
 
-/** 
+/**
  * punto 6 inizializzare un singolo processo
  * pcb_t->p_s ha i seguenti campi:
  * typedef struct state {
