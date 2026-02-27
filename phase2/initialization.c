848aa45ea6b974adbd81acde5e827162e780d51f
CaffeLatteIV <mattiababbini99@gmail.com>
2026-02-27 17:32:57 +0100

    feat: scheduler "funzionante"

--- phase2/initialization.c
+++ phase2/initialization.c
@@ -102,14 +102,18 @@ scheduler(); //dobbiamo ancora fare
 }
 
 void scheduler(){
+  if (!emptyProcQ(&ready_queue)){
+
+  current_process = removeProcQ(&ready_queue); // rimuovo il PCB dalla testa dei ready queue e lo metto come processo corrente (inizio a eseguire il processo)
+  process_counter--;
+  setTIMER(TIMESLICE);
+  klog_print("woooo");
+  bp();
+  LDST(&current_process->p_s);
+  }
   if (process_counter == 0){
     HALT();
   }
-  current_process = removeProcQ(&ready_queue); // rimuovo il PCB dalla testa dei ready queue e lo metto come processo corrente (inizio a eseguire il processo)
-  current_process--;
-  setTIMER(TIMESLICE);
-  LDST(&current_process->p_s);
-
   if (process_counter >0 && soft_block_counter >0){
     setMIE(MIE_ALL  & ~MIE_MTIE_MASK);
     unsigned int status = getSTATUS();
@@ -120,6 +124,5 @@ void scheduler(){
   }else if (process_counter >0 && soft_block_counter ==  0){
     PANIC();
   }
-  bp();
 
 }
