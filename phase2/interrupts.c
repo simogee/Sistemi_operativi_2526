db352e0e4010461e1dcadad315fa695d5685c804
simo <84404752+simogee@users.noreply.github.com>
2026-03-16 18:18:33 +0100

    aggiunto commento interrupts.c

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -1,1 +1,6 @@
 #include "kernel.h"
+
+
+//lower number line higher prio if two interrupt per device should solve the one with highest prio
+//1. PLT highest prio 2. Interval Timer
+//per terminali la trasmissione ha prio piu' alta rispetto ai recv
