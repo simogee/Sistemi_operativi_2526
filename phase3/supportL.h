02bb368a5c2c5ca3f10b7bebb92b08ae19e3dbbb
simo <84404752+simogee@users.noreply.github.com>
2026-08-01 16:35:31 +0200

    spiegazione fase 3 continuata: Evento TLB refill(miss), swap pool table, e TLB_exception_handler(Page faut). Aggiornamento TLB e operazioni del pager. Non risolto il thought challenge

--- phase3/supportL.h
+++ phase3/supportL.h
@@ -23,7 +23,61 @@
  *               ora dobbiamo fare l'or: 0x8003000 or 0x128 = 0x8003128 -> (vpn,asid). Per estrarre: VPN: entryHI >> 12. e per asid: entryHI >> 6(elimina i primi 6 bit) e dobbiamo conservare gli ultimi i 6 bit e basta: and 0x3f. Per fare queste operazioni controllare const.h
  *  Semafori:
  *  Swap pool semaphore -> necessario per evitare confusione e rischio di race condition. eg. un processo rimuove una pagina che serve ad un altro e un'altro rimuove la pagina appena inserita.
- */
+ * 
+ * Storing dev: dal momento che non c'è memoria secondariao si può usare uriscv-mkdev per preloaddare i flash device con i file .out
+ *              Ogni dispositivo quindi rappresenta un processo: viene preconfigurata la memoria del device con l'immagine del processo e il device diventa la memoria secondaria di quel processo
+ * TLB refill:
+ *  uTLB_RefillHandler => si occupa del cache miss event, deve inserire la pagina mancante nel tlb e restartare l'istruzione da prima dell'eccezione generata.(si trova in phase2)
+ *  TLB_refill utilizza il primo frame di RAM come stack page, ha accesso sia alle strutture globali di lvl 2 che a quelle di lvl 3(strutture di supporto).
+ *      Schema miss:
+ *              1.localizzare tramite p_supportStruct la page table:
+ *                 1.1 determinare la page_num "dissezionando" la entryHI nella saved_exception che si trova all'inizio del BIOS datapage.
+ *                 1.2 prelevare la page_num trovata del processo corrente
+ *              2. Scrivere nel TLB con TLBW:
+ *                  2.1 setENTRYHI -> come spiegato sopra
+ *                  2.2 setENTRYLO ->  TODO ma in breve i dati sono già scritti e se V = 0 allora è page fault e bisogna fare tutta la roba del caricamento della pagina
+ *                  2.3 TLBW       ->  scrive tutto sul tlb
+ *              3. Ritornare allo stato precedente con LDST
+ * Swap Pool:
+ *  Zona RAM dove memorizziamo frame -> pagine.
+ *  Si può usare RAM dello stack page utilizzata per test in fase 2(WHAT??) oppure come ultimo frame della memoria starting address is: 0x20020000 (i.e. 0x20000000 + (32 * PAGESIZE)).
+ *  !!Assicurarsi che nella config della macchina sia allocata RAM sufficiente!!
+ * Struttura Swap pool Table:
+ *  ASID, VPN, ptr alla relativa pagina della page_Table del processo.
+ * Possibile usare ASID -1 per indicare frame libero
+ * Size della tabella = size swap pool:1 entry per frame.
+ * Pager:
+ * E' il TLB_exception_handler(la fase 2 passava sopra delle eccezioni da gestire: questa è una di quelle)
+ *  CASI:
+ *      page fault su load op.  Invalid excp. TLBL
+ *      page fault su store op. Invalid excp. TLBS
+ *      Tentativo di scrittura non permessa. Modification excp. Mod -> questa non deve avvenire perchè tutte pagine sono marcate come read/write. Se accade è da gestire come trap.
+ * Schema per gestione Page fault:(!!exception handler di questa fase possono interagire con nucleus e le sue strutture tramite syscall di valore negativo)
+ *      1. ottenere puntatore a current proc support struct.: NSYS8
+ *      2. Determinare la causa del TLB exception -> si trova nella current process support struct sup_exceptstate[0].
+ *      3. Se è un modification bisogna gestirlo come Trap.
+ *      4. Ottenere il mutual exclusion dello swap pool(P sul semaforo della pool)
+ *      5. Determinare la pagina mancante che si trova nel registro entryHI della saved exception state
+ *      6. Prendi un frame libero o da liberare con algoritmo per sceglierlo.
+ *      7. Esaminare il frame:
+ *          7.5 Se occupato: (pagina k del processo x) allora bisogna (atomico)segnare nella page table del processo x la pagina k con V = 0. Updatare TLB se necessario (atomico). Infine bisogna updatare il backing store: si scrive sul dispositivo x il contenuto del frame i appena liberato. Ogni errore è trap.
+ *      8. Leggere il contenuto del backing data del processo corrente corrispondente alla pagina p e scriverlo sul frame i. Ogni errore è trap.
+ *      9. Update la Swap pool table: frame i a pagina p del processo corrente 
+ *      (Atomico)
+ *     10. Update della page table dell current process : aggiornare la pagina p con V= 1 e frame i.
+ *     11. Update del TLB per la entry p.
+ *      (Atomico)
+ *     12. Rilascia il semaforo della swap pool
+ *     13. Ritorna allo stato precedente con LDST
+ * Aggiornamento TLB:
+ *  Ogni aggiornamento di page entry fatta dal pager deve controllare il tlb se quella pagina è presente in tlb.
+ *      Approccio ultimo TLBP -> check se la TLB ha la entry che è stata modificata, dovrebbe tornare l'indice  e va riscritta con TLBWI
+ *      Approccio semplificato: TLBCLR -> cancella tutte le entry(? what??)
+ * Ordine operazioni del Pager:
+ *  Quando aggiorni backing store  prima va fatto  update del page Table e TLB prima di scrittura su bckstr.
+ *  Quando leggi dal backing store, prima lettura di aggiornamento page table e TLB.
+ * Risposta al thought: per la prima, se aggiorno il backing store prima dell'aggiornamento... TODO 5.4 in poi
+ */         
 /*semafori per garantire che processo padre non lasci orfani i figli */
 extern int masterSemaphore; //semaforo del primo processo: viene fatta V solo quando la shell lanciata termina init 0
 extern int shellSemaphore; //semaforo di tutti i processi lanciati dalla shell. viene fatta la V solo quando l'ultimo processo lanciato muore. init 0
