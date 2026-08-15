1f219e13f08066c80057524aff675fd6bee6c275
simo <84404752+simogee@users.noreply.github.com>
2026-08-15 10:50:53 +0200

    legge e stampa da terminale, giusta len

--- testers/shell.c
+++ testers/shell.c
@@ -5,11 +5,8 @@
 
 void main() {
     char stringa[128];
-    SYSCALL(READTERMINAL,(int)&stringa,0,0);
-    int len=0;
-    while((stringa[len] != '\n') && (stringa[len] != '\r')){
-        len++;
-    }
+    int len=SYSCALL(READTERMINAL,(int)stringa,0,0);
+    
     SYSCALL(WRITETERMINAL,(int)stringa,len,0);
     SYSCALL(TERMINATE, 0, 0, 0);
     
